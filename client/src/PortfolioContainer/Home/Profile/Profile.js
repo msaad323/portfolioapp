b3fd7c3bb9cb4e018bac25cb0eb3dfd59fd7017f
Mohd Saad <saad500nbd@gmail.com>
2022-08-26 02:14:02 +0530

    commit for deploy to heroku

--- client/src/PortfolioContainer/Home/Profile/Profile.js
+++ client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -10,9 +10,9 @@ export default function Profile() {
         <div className="profile-details">
           <div className="colz">
             <div className="colz-icon">
-              {/* <a href="https://web.facebook.com/?_rdc=1&_rdr">
+              <a href="https://web.facebook.com/?_rdc=1&_rdr">
                 <i className="fa fa-facebook-square" />
-              </a> */}
+              </a>
               <a href="#">
                 <i className="fa fa-google-plus-square" />
               </a>
@@ -22,9 +22,9 @@ export default function Profile() {
               <a href="https://www.youtube.com/channel/UCSSr5ZDFbilpZ592_ycoAwA">
                 <i className="fa fa-youtube-square" />
               </a>
-              <a href="https://twitter.com/Ehiedu_baba">
+              {/* <a href="https://twitter.com/Ehiedu_baba">
                 <i className="fa fa-twitter" />
-              </a>
+              </a> */}
             </div>
           </div>
           <div className="profile-details-name">
